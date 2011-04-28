94e31f58e47b88833fc286e436841010b2e8d1f9
scott <juveduke@gmail.com>
2011-04-28 00:09:54 -0400

    documented the memoize helper class

--- filmdata/lib/memoize.py
+++ filmdata/lib/memoize.py
@@ -1,17 +1,61 @@
-import functools
+"""
+A simple decorator for memoizing functions and methods.
+"""
+
+from functools import partial as partial
 
 class memoize(object):
+    """
+    Decorator class for memoizing functions and methods.
+    Attributes:
+        func - the function/method that is being decorated
+        cache - the dictionary which stores the arguments passed to the func as
+            keys and the corresponding value that the func returns as the value
+    Usage:
+        @memoize
+        def multiply(x, y):
+            return x * y
+    """
+    
     def __init__(self, func):
+        """
+        Create a new memoize object.
+        Arguments:
+            func - the function to memoize
+        """
         self.func = func
         self.cache = {}
 
     def __call__(self, *args):
+        """
+        Receives the call to the decorated function/method.
+        Arguments:
+            args - all the arguments passed to the function
+        """
         return self.cache_get(args, lambda: self.func(*args))
 
     def __get__(self, obj, objtype):
-        return self.cache_get(id(obj), lambda: self.__class__(functools.partial(self.func, obj)))
+        """
+        Handles the special case for methods in classes.
+        Arguments:
+            obj - the class that contains the decorated method
+            objtype - n/a
+        """
+        return self.cache_get(id(obj),
+                              lambda: self.__class__(partial(self.func,
+                                                             obj)))
 
     def cache_get(self, key, gen_callback):
+        """
+        Searches the cache to see if the result has already been calculated
+        for the given key (the args).  Runs the callback func if the results
+        weren't cached and adds the result to the dictionary.
+        Arguments:
+            key - key to search for in the cache dictionary
+            gen_callback - the callback to run to generate new results
+                and populate the cache
+        Returns the cached (hopefully) result of the gen_callback function
+        """
         if key not in self.cache:
             self.cache[key] = gen_callback()
         return self.cache[key]
