9368e5e43ea7c276407322a3e931b548893869f3
scott <juveduke@gmail.com>
2011-04-28 10:57:38 -0400

    documented some of the sqlalchemy helpers

--- filmdata/lib/sa.py
+++ filmdata/lib/sa.py
@@ -1,19 +1,73 @@
+"""
+Some SQLAlchemy helpers for use in filmdata.
+Includes a special enum type and a generic init function for the models.
+"""
+
 import sqlalchemy as sa
 from filmdata.lib.dotdict import dotdict
 
+def table_class_init(self, **kwargs):
+    """
+    A basic function which takes in keyword arguments and uses them to set
+    the attributes of the instance. Useful for setting the __init__ method
+    of a dynamically created class.
+    """
+    for k,v in kwargs.iteritems():
+        setattr(self, k, v)
+
 class EnumIntType(sa.types.TypeDecorator):
+    """
+    A new enum type for SQLAlchemy which uses the integer type in the 
+    DB for storage. It uses a list to maintain the relationship between
+    the integer (backend) and the string (frontend).
+    Attributes:
+        values - the list of possible values for this column
+        impl - a SQLA thing which denotes what type to use underneath
+            (SmallInteger in this case)
+    Example:
+        #in your table declaration
+        animal_table = sa.Table("title", meta,
+            sa.Column("name", sa.types.Unicode(31), nullable=False),
+            sa.Column("type", EnumIntType(("dog", "cat", "narwhal")), nullable=False))
+
+        #in a query
+        session.query(Animal).filter(Animal.type == "cat").all()
+    """
     impl = sa.types.SmallInteger
 
     values = None
 
     def __init__(self, values=None):
+        """
+        Create a new enum type.
+        Arguments:
+            values - the list of enumerated values
+        """
         sa.types.TypeDecorator.__init__(self)
         self.values = values
 
     def process_bind_param(self, value, dialect):
+        """
+        Runs when the column is trying to be set.  Gets passed one of the
+        strings in the enumerated list and looks up the index of that
+        value in the list.  That integer will then get passed down to
+        the lower levels for storage in the db.
+        Arguments:
+            value - the string value for the column
+            dialect - unused sqla thing
+        """
         return None if value == None else self.values.index(value)
 
     def process_result_value(self, value, dialect):
+        """
+        Getting the result for the column.  This gets passed the raw value
+        (i.e. an integer) from the db and looks it up in the enum list
+        to find the corresponding human readable string that the
+        integer represents.
+        Arguments:
+            value - the string value for the column
+            dialect - unused sqla thing
+        """
         return None if value == None else self.values[value]
 
 class DynamicTables(object):
@@ -90,8 +144,3 @@ class DynamicTables(object):
                       default=datetime.datetime.now,
                       onupdate=datetime.datetime.now),
         ]
-
-
-def table_class_init(self, **kwargs):
-    for k,v in kwargs.iteritems():
-        setattr(self, k, v)
