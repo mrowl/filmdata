f8807733fa7f2aef89f6e7963a089b7fa4d01003
scott <juveduke@gmail.com>
2011-04-27 22:47:48 -0400

    documented the dotdict class

--- filmdata/lib/dotdict.py
+++ filmdata/lib/dotdict.py
@@ -1,7 +1,20 @@
+"""
+For accessing dictionaries with dot notation.
+"""
 class dotdict(dict):
+    """
+    This class operates like the standard dictionary except you can use
+    dot notation for accessing values.
+    Example:
+        my_dict = dotdict()
+        my_dict['thing'] = 'what'
+        my_dict.thing  # 'what'
+        my_dict['thing']  # 'what'
+    """
 
     def __getattr__(self, attr):
+        """Wraps around the normal dictionary getter"""
         return self.get(attr, None)
 
-    __setattr__= dict.__setitem__
-    __delattr__= dict.__delitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
