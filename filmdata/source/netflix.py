dbaacabbfe5f85f13ca7d99c0fa1e3d425260c8d
scott <juveduke@gmail.com>
2011-06-24 20:49:35 -0400

    added fetching for votes to netflix. it scrapes the netflix web site.

--- filmdata/source/netflix.py
+++ filmdata/source/netflix.py
@@ -2,8 +2,15 @@ import logging, re, HTMLParser, os, string
 from datetime import datetime
 from decimal import Decimal
 import oauth2 as oauth
+from functools import partial
 
 import xml.etree.cElementTree as etree
+from tornado import httpclient, httputil
+import tornado.ioloop
+from cookielib import MozillaCookieJar
+from Cookie import SimpleCookie
+
+import json
 
 from filmdata import config
 
@@ -81,10 +88,88 @@ class Fetch(NetflixMixin):
     _titles_url = config.netflix.titles_url
     _title_url_base = config.netflix.title_url_base
     _call_limit = 4900
+    _master_lock = False
+    _thread_finished_count = 0
+    _thread_count = 50
 
     @classmethod
     def fetch_data(cls):
         cls._download_title_catalog()
+
+    @classmethod
+    def fetch_votes(cls):
+        re_link = re.compile('^<link href="(http://www.netflix.com/Movie/[^/]+/[0-9]+)" rel="alternate" title="web page"/>$')
+        re_votes = re.compile('^\s+Average of ([0-9,]+) ratings:\s*$')
+        re_key = re.compile('^http://www.netflix.com/Movie/[^/]+/([0-9]+)')
+        headers = httputil.HTTPHeaders()
+
+        jar = MozillaCookieJar(config.netflix.cookies_txt_path)
+        jar.load()
+        for line in jar:
+            cookie = SimpleCookie()
+            cookie[line.name] = line.value
+            for key in ('domain', 'expires', 'path'):
+                cookie[line.name][key] = getattr(line, key)
+            partitions = cookie.output().partition(' ') 
+            headers.add(partitions[0].replace('Set-', ''), partitions[2])
+
+        urls = []
+        for line in open(cls._titles_file_path):
+            link_match = re_link.match(line.strip())
+            if link_match and link_match.group(1):
+                key = re_key.match(link_match.group(1)).group(1)
+                urls.append((int(key),
+                             link_match.group(1).replace('//www.',
+                                                         '//movies.')))
+                #if len(urls) > 20:
+                    #break
+
+        master = {}
+        def on_response(resp, prev_key=None, thread_urls=None,
+                        thread_results=None, client=None):
+            if resp is None:
+                log.info('Starting thread')
+            elif resp.error:
+                log.error("Error: %s" % str(resp.error))
+            else:
+                for line in resp.buffer:
+                    votes_match = re_votes.match(line)
+                    if votes_match and votes_match.group(1):
+                        thread_results[prev_key] = int(votes_match.group(1).replace(',', ''))
+            try:
+                next = thread_urls.next()
+                client.fetch(next[1],
+                             partial(on_response, prev_key=next[0],
+                                     thread_urls=thread_urls,
+                                     thread_results=thread_results,
+                                     client=client),
+                             headers=headers)
+            except StopIteration:
+                #if not tornado.ioloop.IOLoop.instance()._callbacks:
+                    #tornado.ioloop.IOLoop.instance().stop()
+                log.info('Done this thread (trailing key = %d)' % prev_key)
+                start = True
+                while start or cls._master_lock:
+                    if not cls._master_lock:
+                        cls._master_lock = True
+                        master.update(thread_results)
+                        cls._master_lock = False
+                    start = False
+                cls._thread_finished_count += 1
+                if cls._thread_finished_count == cls._thread_count:
+                    tornado.ioloop.IOLoop.instance().stop()
+                    json.dump(master, open(config.netflix.votes_json_path, 'w'))
+
+        ioloop = tornado.ioloop.IOLoop.instance()
+        for i, url in enumerate(urls):
+            if i < cls._thread_count:
+                thread_urls = iter([ y for x, y in enumerate(urls) if
+                                     x % cls._thread_count == i % cls._thread_count ])
+                client = httpclient.AsyncHTTPClient()
+                ioloop.add_callback(partial(on_response, None, None, thread_urls, {},
+                                            client))
+        ioloop.start()
+
 
     @classmethod
     def _download_title_catalog(cls):
@@ -138,7 +223,6 @@ class Produce(NetflixMixin):
     @classmethod
     def produce_titles(cls, types):
         for title in cls._get_titles(types):
-            title['master'] = True
             yield title
 
     @classmethod
@@ -160,6 +244,7 @@ class Produce(NetflixMixin):
             for k in avail.keys():
                 if avail[k] != None:
                     avail[k] = datetime.fromtimestamp(int(avail[k]))
+
             quality = format.find('./category[@scheme='
                                   '"http://api.netflix.com/categories/title_formats/quality"]')
             if quality != None and quality.get('label') == 'HD':
@@ -169,6 +254,10 @@ class Produce(NetflixMixin):
             runtime_node = elem.find('runtime')
             if runtime_node != None:
                 avail['runtime'] = int(runtime_node.text)
+
+            mpaa = format.find('./category[@sheme="http://api.netflix.com'
+                               '/categories/mpaa_ratings"]')
+            avail['mpaa'] = mpaa.get('label') if mpaa != None else None
 
             label_to_key_map = {
                 'Blu-ray' : 'bluray',
@@ -190,7 +279,7 @@ class Produce(NetflixMixin):
             avails = {}
             for avail in [ a for a in map(_get_availability, elems) if a ]:
                 avails.update(avail)
-            return avails if len(avails) > 0 else None
+            return avails
                 
         def _get_art(elem):
             box_art = elem.find('./link[@rel="http://schemas.netflix.com'
@@ -221,7 +310,7 @@ class Produce(NetflixMixin):
                               '/catalog/titles/synopsis.short"]/short_synopsis')
             if short != None:
                 synopsis['short'] = clean(short.text)
-            return synopsis if len(synopsis) > 0 else None
+            return synopsis
 
         def _get_genres(elem):
             categories = elem.findall('./category')
@@ -230,19 +319,27 @@ class Produce(NetflixMixin):
                 scheme = cat.get('scheme')
                 if 'genres' in scheme:
                     genres.append(clean(cat.get('label')))
-            return genres if len(genres) > 0 else None
+            return genres
 
-        def _form_title_dict(elem, netflix_key):
+        def _form_title_dict(elem, key, votes=None):
             release_year = elem.find('release_year')
             if release_year == None or release_year.text == None:
-                log.info('Year not found on %d' % netflix_key)
+                log.info('Year not found on %d' % key)
                 return None
+            link = elem.find('./link[@rel="http://schemas.netflix.com'
+                             '/catalog/title/ref.tiny"]')
+            href = { 'tiny' : link.get('href') } if link != None else {}
 
             title = {
-                'key' : netflix_key,
+                'key' : key,
                 'name' : clean(elem.find('title').get('regular')), 
                 'year' : int(release_year.text),
-                'rating' : Decimal(elem.find('average_rating').text) * cls._rating_factor,
+                'href' : href,
+                'stat' : {
+                    'votes' : votes,
+                    'rating' : (Decimal(elem.find('average_rating').text) *
+                                cls._rating_factor),
+                },
                 'synopsis' : _get_synopsis(elem),
                 'availability' : _get_availabilities(elem),
                 'art' : _get_art(elem),
@@ -250,6 +347,11 @@ class Produce(NetflixMixin):
             }
 
             return title
+
+        if os.path.exists(config.netflix.votes_json_path):
+            votes = json.load(open(config.netflix.votes_json_path))
+        else:
+            votes = {}
 
         context = etree.iterparse(cls._titles_file_path,
                                   events=('start', 'end'))
@@ -259,7 +361,12 @@ class Produce(NetflixMixin):
             if event == 'end' and elem.tag == 'catalog_title':
                 film_match = cls._re_film_test.match(elem.find('id').text)
                 if film_match:
-                    title = _form_title_dict(elem, int(film_match.group(1)))
+                    str_key = film_match.group(1)
+                    if str_key in votes:
+                        vote = votes[str_key]
+                    else:
+                        vote = None
+                    title = _form_title_dict(elem, int(str_key), vote)
                     if title != None:
                         yield title
                 elem.clear()
