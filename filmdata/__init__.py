01d67de01e700ceb9388974a3f859a9818d86824
scott <juveduke@gmail.com>
2011-07-11 23:51:02 -0400

    added more genre massagers and moved the genre list to the config

--- filmdata/__init__.py
+++ filmdata/__init__.py
@@ -18,3 +18,29 @@ for section in raw_config.sections():
 
 config['TITLE_TYPES'] = ('film', 'tv')
 config['ROLE_TYPES'] = ('director', 'actor', 'actress', 'producer', 'writer')
+config['sources'] = config.core.active_sources.split()
+config['role_types'] = config.core.active_role_types.split()
+config['role_groups'] = config.core.active_role_groups.split()
+config['cast_roles'] = set(config['role_types']) & set(('actor', 'actress'))
+
+config['genre_to_bit_map'] = {
+    'drama' : 0,
+    'comedy' : 1,
+    'short' : 2,
+    'foreign' : 3,
+    'horror' : 4,
+    'documentary' : 5,
+    'action' : 6,
+    'adventure' : 7,
+    'thriller' : 8,
+    'romance' : 9,
+    'crime' : 10,
+    'family' : 11,
+    'sci-fi' : 12,
+    'fantasy' : 13,
+    'mystery' : 14,
+    'musical' : 15,
+    'war' : 16,
+    'western' : 17,
+    'indie' : 18,
+}
--- filmdata/genre.py
+++ filmdata/genre.py
@@ -1,26 +1,6 @@
 import re
 
-GENRE_BIT_MAP = {
-    'drama' : 0,
-    'comedy' : 1,
-    'short' : 2,
-    'foreign' : 3,
-    'horror' : 4,
-    'documentary' : 5,
-    'action' : 6,
-    'adventure' : 7,
-    'thriller' : 8,
-    'romance' : 9,
-    'crime' : 10,
-    'family' : 11,
-    'sci-fi' : 12,
-    'fantasy' : 13,
-    'mystery' : 14,
-    'musical' : 15,
-    'war' : 16,
-    'western' : 17,
-    'indie' : 18,
-}
+from filmdata import config
 
 class Genres:
     _splitters = [
@@ -29,56 +9,120 @@ class Genres:
         re.compile('^(indie) (.+?)$'),
         re.compile('^(romantic) (.+?)$'),
         re.compile('^(classic) (.+?)$'),
-        re.compile('^(crime) (dramas)$'),
-        re.compile('^(sports) (dramas)$'),
-        re.compile('^(social issue) (dramas)$'),
+        re.compile('^(.+?) (dramas)$'),
+        re.compile('^(dark humor) & (black comedies)$'),
+        re.compile('^(.+?) (comedies)$'),
+        re.compile('^(extreme combat) (mixed martial arts)$'),
+        re.compile('^(.+?) based on (.+?)$'),
         re.compile('^(.+?) (period pieces)$'),
         re.compile('^(.+?) (thrillers)$'),
         re.compile('^(.+?) (classics)$'),
         re.compile('^(.+?) (documentaries)$'),
+        re.compile('^(crime) (action)$'),
+        re.compile('^(espionage) (action)$'),
+        re.compile('^(military & war) (action)$'),
+        re.compile('^(sci-fi) (adventure)$'),
+        re.compile('^(sci-fi) (cult)$'),
+        re.compile('^(sci-fi) (horror)$'),
+        re.compile('^(action) (sci-fi & fantasy)$'),
+        re.compile('^(alien) (sci-fi)$'),
     ]
 
     _extractors = [
         re.compile('^(.+?) language$'),
         re.compile('^(sports) & fitness$'),
-        re.compile('^action & (adventure)$'),
+        re.compile('^(sports) stories$'),
+        re.compile('^(war) stories$'),
+        re.compile('^(inspirational) stories$'),
+        re.compile('^(silent) films$'),
+        re.compile('^(heist) films$'),
+        re.compile('^(heist) films$'),
+        re.compile('^(animation) for grown-ups$'),
     ]
 
     _mappers = {
+        'ages 5-7' : 'family',
+        'ages 8-10' : 'family',
+        'ages 11-12' : 'family',
+        'children & family' : 'family',
+        'kids & family' : 'family',
+        'science fiction & fantasy' : 'sci-fi & fantasy',
+        'science fiction' : 'sci-fi',
         'period piece' : 'period',
         'period pieces' : 'period',
+        'spoofs and satire' : 'spoofs & satire',
+        'spoofs' : 'spoof',
         'dramas' : 'drama',
+        'epics' : 'epic',
         'adventures' : 'adventure',
         'comedies' : 'comedy',
+        'black comedies' : 'black comedy',
         'thrillers' : 'thriller',
         'music & musicals' : 'musical',
         'musicals' : 'musical',
-        'children & family' : 'family',
         'romantic' : 'romance',
         'classics' : 'classic',
+        'classical instrumental music' : 'classical',
+        'classical music' : 'classical',
         'documentaries' : 'documentary',
         'historical' : 'history',
         'biographical' : 'biography',
+        'biographies' : 'biography',
         'sports' : 'sport',
         'westerns' : 'western',
+        'film-noir' : 'film noir',
+        'spain' : 'spanish',
+        'france' : 'french',
+        'italy' : 'italian',
+        'germany' : 'german',
+        'korea' : 'korean',
+        'japan' : 'japanese',
+        'russia' : 'russian',
     }
 
-    _priority = [ k for k, v in sorted(GENRE_BIT_MAP.items(), key=lambda i: i[1]) ]
+    _overriders = {
+        'sci-fi & fantasy' : ('sci-fi', 'fantasy'),
+        'action & adventure' : ('action', 'adventure'),
+        'mystery & suspense' : ('mystery', 'suspense'),
+        'suspense & thriller' : ('thriller', 'suspense'),
+        'military & war' : ('military', 'war'),
+    }
+
+    _ignored = frozenset((
+        'miscellaneous',
+        'dark humor',
+        'must-see',
+        'the book',
+    ))
+
+    _priority = [ k for k, v in sorted(config.genre_to_bit_map.items(), key=lambda i: i[1]) ]
 
     def __init__(self, names):
-        self.names = [ n.lower() for n in names ]
-        self.labels = []
-        self._build_list()
+        first_pass = self._build_list([ n.lower() for n in names ])
+        second_pass = self._build_list(first_pass)
+        third_pass = self._build_list(second_pass)
+        self.labels = self._order_set(third_pass)
 
-    def _build_list(self):
-        split = self._run_splits(self.names)
+    def _build_list(self, names):
+        split = self._run_splits(names)
         extracted = self._run_extractions(split)
         mapped = self._run_mappers(extracted)
-        self.labels = self._order_set(mapped)
+        overridden = self._run_overriders(mapped)
+        return set([ g for g in overridden if not g in self._ignored ])
 
     def _order_set(self, names):
         ordered = [ p for p in self._priority if p in names ]
         return ordered + [ n for n in names if not n in ordered ]
+    
+    def _run_overriders(self, names):
+        buffer = set()
+        for name in list(names):
+            for over in self._overriders.get(name, []):
+                if over in names:
+                    break
+            else:
+                buffer.add(name)
+        return buffer
 
     def _run_mappers(self, names):
         buffer = set()
